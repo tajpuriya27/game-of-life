0f9149ac1d74a5f1fe5cedc54bec1158b288fec5
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-07 15:30:06 +0545

    Main file completed
    Need to check why clear function is clearing interval

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
 const width = 25;
-const height = 20; // width and height dimensions of the board
-
-/**
- * Create a Game of Life instance
- */
+const height = 20;
+let playIntervalId;
 
 const gol = new GameOfLife(width, height);
 
-
-/**
- * create a table and append to the DOM
- */
-
-// Actual table cells
 const tds = [];
 
-// <table> element
 const table = document.createElement("tbody");
-// build a table row <tr>
+
 for (let h = 0; h < height; h++) {
   const tr = document.createElement("tr");
-  // build a table column <td>
   for (let w = 0; w < width; w++) {
     const td = document.createElement("td");
-    // We'll put the coordinates on the cell
-    // Element itself (using dataset),
-    // letting us fetch it in a click listener later.
     td.dataset.row = h;
     td.dataset.col = w;
     tds.push(td);
@@ -35,50 +21,56 @@ for (let h = 0; h < height; h++) {
 }
 document.getElementById("board").append(table);
 
-
-/**
- * Draws every cell from the gol instance into an actual, visible DOM element
- */
-
 const paint = () => {
-  // TODO:
-  //   1. For each <td> in the table:
-  //     a. If its corresponding cell in gol instance is alive,
-  //        give the <td> the `alive` CSS class.
-  //     b. Otherwise, remove the `alive` class.
-  //
-  // To find all the <td>s in the table, you might query the DOM for them, or you
-  // could choose to collect them when we create them in createTable.
-  //
-  // HINT:
-  //   https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  //   https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByTagName
+  let k = 0;
+  for (let h = 0; h < height; h++) {
+    for (let w = 0; w < width; w++) {
+      gol.board[h][w]
+        ? tds[k].classList.add("alive")
+        : tds[k].classList.remove("alive");
+      k++;
+    }
+  }
+};
+
+document.getElementById("board").addEventListener("click", (event) => {
+  if (event.target.tagName === "TD") {
+    let row = event.target.dataset.row;
+    let col = event.target.dataset.col;
+    gol.board[row][col] = event.target.classList.contains("alive") ? 0 : 1;
+    event.target.classList.toggle("alive");
+  }
+});
+
+document.getElementById("step_btn").addEventListener("click", stepFun);
+
+function stepFun() {
+  let old_board = gol.board;
+  gol.tick();
+  let new_board = gol.board;
+  paint();
+  if (JSON.stringify(old_board) === JSON.stringify(new_board)) {
+    clearInterval(playIntervalId);
+  }
+  console.log("interval called");
 }
 
-
-/**
- * Event Listeners
- */
-
-document.getElementById("board").addEventListener("click", event => {
-  // TODO: Toggle clicked cell (event.target) and paint
+document.getElementById("play_btn").addEventListener("click", (event) => {
+  playIntervalId = setInterval(stepFun, 1000);
 });
 
-document.getElementById("step_btn").addEventListener("click", event => {
-  // TODO: Do one gol tick and paint
-});
-
-document.getElementById("play_btn").addEventListener("click", event => {
-  // TODO: Start playing by calling `tick` and paint
-  // repeatedly every fixed time interval.
-  // HINT:
-  // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
-});
-
-document.getElementById("random_btn").addEventListener("click", event => {
+document.getElementById("random_btn").addEventListener("click", (event) => {
   // TODO: Randomize the board and paint
+  for (let h = 0; h < height; h++) {
+    for (let w = 0; w < width; w++) {
+      gol.board[h][w] = Math.round(Math.random() * 1);
+    }
+  }
+  paint();
+  console.log("random clicked");
 });
 
-document.getElementById("clear_btn").addEventListener("click", event => {
-  // TODO: Clear the board and paint
+document.getElementById("clear_btn").addEventListener("click", (event) => {
+  gol.board = gol.makeBoard();
+  paint();
 });
